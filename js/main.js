289e95403d77685c17df7e841cb81c74ac7b57c6
ahesantitu <77657906+ahesantitu@users.noreply.github.com>
2021-04-06 18:03:52 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,47 @@
+/*====================================
+Humberger Menu
+======================================*/
+document.querySelector('.hamburger-menu').addEventListener('click', () => {
+document.querySelector('.container').classList.toggle('change');
+});
+
+
+function main() {
+
+(function () {
+   'use strict';
+   $('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 40
+            }, 900);
+            return false;
+          }
+        }
+      });
+
+    /*====================================
+    Show Menu Top 
+    ======================================*/
+    $(window).bind('scroll', function() {
+        var navHeight = $(window).height() - 100;
+        if ($(window).scrollTop() > navHeight) {
+            $('.navbar-default').addClass('on');
+        } else {
+            $('.navbar-default').removeClass('on');
+        }
+    });
+
+    $('body').scrollspy({ 
+        target: '.navbar-default',
+        offset: 80
+    })
+
+}());
+
+
+}
+main();
